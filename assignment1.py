898f1353bc8bc6fc797f752d3f421cfe1d460c2a
rathorenikhil678-source <rathorenikhil678@gmail.com>
2026-05-19 19:58:27 +0530

    Added assignment code

---
+++ assignment1.py
@@ -0,0 +1,21 @@
+
+name = input("Enter student name: ")
+student_class = input("Enter class: ")
+
+
+mark1 = float(input("Enter marks for Subject 1: "))
+mark2 = float(input("Enter marks for Subject 2: "))
+mark3 = float(input("Enter marks for Subject 3: "))
+mark4 = float(input("Enter marks for Subject 4: "))
+mark5 = float(input("Enter marks for Subject 5: "))
+
+
+total = mark1 + mark2 + mark3 + mark4 + mark5
+percentage = (total / 500) * 100
+
+# Display result
+print("\n----- Student Result -----")
+print("Student Name :", name)
+print("Class :", student_class)
+print("Total Marks :", total)
+print("Percentage :", percentage, "%")
