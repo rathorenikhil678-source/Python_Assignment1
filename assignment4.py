4f97147f43ca83403e009388a0687b8a637b1536
rathorenikhil678-source <rathorenikhil678@gmail.com>
2026-05-19 20:11:59 +0530

    Added Assignment 4

---
+++ assignment4.py
@@ -0,0 +1,27 @@
+# Assignment Operators Practice
+
+a = 10
+print("Initial value of a =", a)
+
+
+a += 5
+print("After += :", a)
+
+a -= 3
+print("After -= :", a)
+
+a *= 2
+print("After *= :", a)
+
+a /= 4
+print("After /= :", a)
+
+a %= 3
+print("After %= :", a)
+
+a **= 2
+print("After **= :", a)
+
+
+a //= 2
+print("After //= :", a)
